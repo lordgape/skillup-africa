a6f788a08bca021d64dec321efe9bcf8613f70e6
Chesvic Lordgape <lordgape@gmail.com>
2021-05-05 04:21:58 +0100

    Added a streaming server

--- week-8-05-friday/07-assignment.js
+++ week-8-05-friday/07-assignment.js
@@ -1,1 +1,15 @@
 // Create a video server that allows your friend to stream the latest movie on your website.
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path")
+
+const server = http.createServer((req,res) => {
+    res.writeHead(200,{"Content-Type" : "video/mp4"})
+    const readableStream = fs.createReadStream(path.join(__dirname,"spirit.mp4"));
+    readableStream.pipe(res);
+})
+
+server.listen(3000,() => {
+    console.log(`Server is up and running`);
+})
