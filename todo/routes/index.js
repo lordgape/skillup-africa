9e7b748f8d510a63947b03f0de1986756b23912a
Chesvic Lordgape <lordgape@gmail.com>
2021-05-14 22:12:01 +0100

    Create Views for todos

--- todo/routes/index.js
+++ todo/routes/index.js
@@ -1,25 +1,27 @@
 const todoRoute = require('./todoRoutes');
 
+const FrontendTodoController = require('../controllers/FrontendTodoController');
+
 module.exports = (app) => {
   app.use('/health', (req, res) => {
     res.send("I'm in a good working condition");
   });
 
-  app.use('/todos', todoRoute);
+  app.use('/api/v1/todos', todoRoute);
+
+  app.use('/todos', async (req, res) => {
+    FrontendTodoController.getAllTodos(req, res);
+  });
 
   app.get('/', (req, res) => {
-    console.log(`I go here`);
-    res.json({"success": 'You just Ping me !'});
+    res.json({ success: 'You just Ping me !' });
   });
 };
-
-
 
 // Route
 // middleware use function with route pattern and handler
 // http verb of the express function
 // middleware use function with route pattern and exported router module
-
 
 // User Module
 // Post Module
